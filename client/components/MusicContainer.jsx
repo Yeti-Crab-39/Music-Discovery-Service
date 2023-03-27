0bc0825a6f816e89b814bcb4663edec0863c882b
Jeb Stone <jeb.b.stone@gmail.com>
2023-03-27 13:39:39 -0600

    this will allow song to be deleted from topTen

--- client/components/MusicContainer.jsx
+++ client/components/MusicContainer.jsx
@@ -10,16 +10,13 @@ export default function MusicContainer() {
     uri: '25aawb25AiIdfj5ctvr3Do',
   });
   //<iframe src="https://open.spotify.com/embed/track/2Y8D1aBfSngGN41XnOwydy?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-  const [topTenSongs, setTopTenSongs] = useState([
-    //need this to reflect what is in the database somehow
-    // { song: 'Jebs 2nd Song', artist: 'john', uri: 'test' },
-    // { song: 'Kudrat', artist: 'Matt', uri: 'secondTest' },
-    // { song: 'Jebs 2nd Song', artist: 'jeb', uri: 'jeb.com' },
-    // { song: 'LESSGOO', artist: 'Yeti Crab', uri: 'teststring here' },
-  ]);
-  useEffect(() => {
+  const [topTenSongs, setTopTenSongs] = useState([]);
+  // useEffect(() => {
+  //   //  search music schema
 
-  }, [topTenSongs])
+  //   // add all the songs to the state
+
+  // }, [topTenSongs])
   return (
     <div>
       <MusicPlayer songState={songState} />
